709e6b9f142de0a2464ed2220d2329063cf9f6cb
Yamamoto Masao <yamamoto.masa-y@jp.fujitsu.com>
2018-04-11 17:47:49 +0900

    Lesson6課題
    アイテム廃棄＆動的生成

---
+++ Assets/Scripts/ItemDestroyer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+		//メインカメラ位置を基準にアイテムを削除
+		GameObject maincamera = GameObject.FindWithTag("MainCamera");
+		if (this.transform.position.z < maincamera.transform.position.z) {
+			Destroy (this.gameObject);
+		}
+	}
+}
